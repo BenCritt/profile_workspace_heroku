8655f05775a3d3385752119c615eb978863fa55e
BenCritt <130415312+BenCritt@users.noreply.github.com>
2023-10-02 12:03:22 -0500

    I added code to save a file to client.

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 from .forms import ReviewForm
 import os
 import qrcode
+from django.http import HttpResponse
+
+# from django.http import HttpResponse
 
 
 def home(request):
@@ -29,6 +32,11 @@ def qr_code_generator(request):
             filename = f"qrcode_{data}.png"
             full_path = os.path.join(save_dir, filename)
             img.save(full_path)
+
+            with open(full_path, "rb") as f:
+                response = HttpResponse(f.read(), content_type="image/png")
+                response["Content-Disposition"] = 'attachment; filename="qrcode.png"'
+                return response
             """
             print(
                 "The QR code was successfully created and downloaded to the default downloads folder."
