99059e6eabe40a70c7fe654c865b1a2b5ccbd794
BenCritt <130415312+BenCritt@users.noreply.github.com>
2023-10-17 12:56:23 -0500

    I added the option for users to use a second data range in the Monte Carlo Simulator.

--- projects/forms.py
+++ projects/forms.py
@@ -17,15 +17,35 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class MonteCarloForm(forms.Form):
     sim_quantity = forms.IntegerField(
-        label="Number of Simulations: ",
+        label="Number of Simulations ",
         validators=[
-            MinValueValidator(1, message="Value must be at least 1."),
+            MinValueValidator(
+                1, message="The number of simulations must be at least 1."
+            ),
             MaxValueValidator(
                 1000000,
                 message="The number of simulations cannot exceed 1,000,000.  This limit ensures server resources aren't overused.",
             ),
         ],
     )
-    min_val = forms.FloatField(label="Minimum Value: ")
-    max_val = forms.FloatField(label="Maximum Value: ")
-    target_val = forms.FloatField(label="Target Value: ")
+    min_val = forms.FloatField(label="Minimum Value")
+    max_val = forms.FloatField(label="Maximum Value")
+    target_val = forms.FloatField(label="Target Value")
+
+    second_sim_quantity = forms.IntegerField(
+        required=False,
+        label="Second Number of Simulations",
+        validators=[
+            MinValueValidator(
+                1,
+                message="The value for second number of simulations, if you wish to include a second data range, must be at least 1.  If you only want one data range in your graph, please leave the secondary fields blank.",
+            ),
+            MaxValueValidator(
+                1000000,
+                message="The number of second simulations cannot exceed 1,000,000.  This limit ensures server resources aren't overused.",
+            ),
+        ],
+    )
+    second_min_val = forms.FloatField(label="Second Minimum Value", required=False)
+    second_max_val = forms.FloatField(label="Second Maximum Value", required=False)
+    second_target_val = forms.FloatField(label="Second Target Value", required=False)
--- projects/views.py
+++ projects/views.py
@@ -62,6 +62,7 @@ def monte_carlo_simulator(request):
     if request.method == "POST":
         form = MonteCarloForm(request.POST)
         if form.is_valid():
+            # This determines where to save the PDF file that will eventually be created.
             home_dir = os.path.expanduser("~")
             save_dir = os.path.join(home_dir, "Downloads")
             if not os.path.exists(save_dir):
@@ -69,34 +70,63 @@ def monte_carlo_simulator(request):
             filename = "probability_graph.pdf"
             full_path = os.path.join(save_dir, filename)
 
+            # This pulls the data from the HTML to prepare for the graph generation.
             sim_quantity = form.cleaned_data["sim_quantity"]
             min_val = form.cleaned_data["min_val"]
             max_val = form.cleaned_data["max_val"]
             target_val = form.cleaned_data["target_val"]
 
             sim_result = np.random.uniform(min_val, max_val, sim_quantity)
+            second_sim_quantity = form.cleaned_data["second_sim_quantity"]
+            # Begin second data range, if there is one.
+            if form.cleaned_data["second_sim_quantity"] is not None:
+                second_min_val = form.cleaned_data["second_min_val"]
+                second_max_val = form.cleaned_data["second_max_val"]
+                second_target_val = form.cleaned_data["second_target_val"]
 
-            plt.figure()
-            plt.hist(sim_result, density=True, edgecolor="white")
-            plt.axvline(target_val, color="r")
-            plt.savefig(full_path, format="pdf")
+                # Generate data for the second range.
+                second_sim_result = np.random.uniform(
+                    second_min_val, second_max_val, second_sim_quantity
+                )
 
-            with open(full_path, "rb") as f:
-                response = HttpResponse(f.read(), content_type="pdf")
-                response[
-                    "Content-Disposition"
-                ] = 'attachment; filename="probability_graph.pdf"'
-                return response
+                # Create the visual graph.
+                plt.figure()
+                plt.hist(sim_result, density=True, edgecolor="white")
+                plt.axvline(target_val, color="r")
+                if second_target_val != None:
+                    plt.hist(
+                        second_sim_result, density=True, edgecolor="white", alpha=0.5
+                    )
+                    plt.axvline(second_target_val, color="b")
+                else:
+                    plt.hist(
+                        second_sim_result, density=True, edgecolor="white", alpha=0.5
+                    )
+                plt.savefig(full_path, format="pdf")
 
-            probability = (sim_result > target_val).sum() / sim_quantity
+                with open(full_path, "rb") as f:
+                    response = HttpResponse(f.read(), content_type="pdf")
+                    response[
+                        "Content-Disposition"
+                    ] = 'attachment; filename="probability_graph.pdf"'
+                    return response
 
-            return render(
-                request,
-                "projects/monte_carlo_simulator.html",
-                {"probability": probability},
-            )
+            elif form.cleaned_data["second_sim_quantity"] is None:
+                plt.figure()
+                plt.hist(sim_result, density=True, edgecolor="white")
+                plt.axvline(target_val, color="r")
+                plt.savefig(full_path, format="pdf")
+
+                with open(full_path, "rb") as f:
+                    response = HttpResponse(f.read(), content_type="pdf")
+                    response[
+                        "Content-Disposition"
+                    ] = 'attachment; filename="probability_graph.pdf"'
+                    return response
+
     else:
         form = MonteCarloForm()
+
     return render(
         request, "projects/monte_carlo_simulator.html", context={"form": form}
     )
