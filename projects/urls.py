4882d2a1a2d526f7e2aeee7461c2a6228e236c42
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-02-18 17:44:13 -0600

    Improving how my site is indexed.

--- projects/urls.py
+++ projects/urls.py
@@ -12,18 +12,18 @@ sitemaps = {
 }
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    # path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("resume/", views.resume, name="resume"),
     path("qr_code_generator/", views.qr_code_generator, name="qr_code_generator"),
     path("contact/", views.contact, name="contact"),
-    path("", views.home, name="home"),
     path(
         "monte_carlo_simulator/",
         views.monte_carlo_simulator,
         name="monte_carlo_simulator",
     ),
     path("weather/", views.weather, name="weather"),
-    path("weather_results/", views.weather, name="weather"),
+    path("weather_results/", views.weather, name="weather_results"),
     path("all_projects/", views.all_projects, name="all_projects"),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
