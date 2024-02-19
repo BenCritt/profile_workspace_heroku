b6b3ae9930d5ef47353013b2940bdf950acbb6c6
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-02-18 18:26:00 -0600

    Debugging an issue.

--- my_profile/settings.py
+++ my_profile/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8r3!59w$m@=4692d!+*)z$=em068%#kxmj#s98_akxrudag*-#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
