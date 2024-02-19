31bd87ad0871180753bf17903b02f4164fec34d8
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-02-18 18:28:58 -0600

    Improving how my site generates sitemaps.

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib.sitemaps.views import sitemap
 from .sitemap import StaticViewSitemap, RootSitemap
 
-register_converter(HTTPSConverter, "https")
+# register_converter(HTTPSConverter, "https")
 
 app_name = "projects"
 
@@ -15,23 +15,17 @@ sitemaps = {
 
 urlpatterns = [
     # path("home/", views.home, name="home"),
-    path("", views.home, name="home", scheme="https"),
-    path("resume/", views.resume, name="resume", scheme="https"),
-    path(
-        "qr_code_generator/",
-        views.qr_code_generator,
-        name="qr_code_generator",
-        scheme="https",
-    ),
-    path("contact/", views.contact, name="contact", scheme="https"),
+    path("", views.home, name="home"),  # , scheme="https"
+    path("resume/", views.resume, name="resume"),
+    path("qr_code_generator/", views.qr_code_generator, name="qr_code_generator"),
+    path("contact/", views.contact, name="contact"),
     path(
         "monte_carlo_simulator/",
         views.monte_carlo_simulator,
         name="monte_carlo_simulator",
-        scheme="https",
     ),
-    path("weather/", views.weather, name="weather", scheme="https"),
-    path("weather_results/", views.weather, name="weather_results", scheme="https"),
-    path("all_projects/", views.all_projects, name="all_projects", scheme="https"),
+    path("weather/", views.weather, name="weather"),
+    path("weather_results/", views.weather, name="weather_results"),
+    path("all_projects/", views.all_projects, name="all_projects"),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
