71a7e4011e5349739688194a02024f1df903ce57
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-03-12 18:21:56 -0500

    I'm cleaning up my code and adding more comments.

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from pyzipcode import ZipCodeDatabase
-from django import forms
 from django.core.validators import (
     MinValueValidator,
     MaxValueValidator,
@@ -10,16 +9,22 @@ from django.core.validators import (
 )
 
 
+# This is the form for the Grade Level Text Analyzer.
 class TextForm(forms.Form):
+    # Create a CharField to hold the text input, using a textarea widget for multiline input.
     text = forms.CharField(
+        # Configure the textarea widget with Bootstrap class for styling and set its size.
         widget=forms.Textarea(
             attrs={"class": "form-control", "rows": 5, "style": "max-width: 500px;"}
         ),
-        label="Enter text to analyze",
+        label="Enter text to analyze",  # Set the label for the text input field.
+        # Add validators to ensure the text length is within the specified range.
         validators=[
+            # Minimum length validator: Text must be at least 1200 characters.
             MinLengthValidator(
                 1200, message="The sample you've provided is too short."
             ),
+            # Maximum length validator: Text must not exceed 10000 characters.
             MaxLengthValidator(
                 10000, message="The sample you've provided is too long."
             ),
@@ -27,17 +32,23 @@ class TextForm(forms.Form):
     )
 
 
+# This is the form for the QR Code Generator.
 class QRForm(forms.Form):
+    # Create a CharField to hold the text input for the QR code.
     qr_text = forms.CharField(
-        label="",
-        max_length=8000,
+        label="",  # The label is set to an empty string for a cleaner UI without a field label.
+        max_length=8000,  # Set the maximum length of the text input to 8000 characters.
+        # Configure the TextInput widget with a custom class for styling and set the size attribute.
         widget=forms.TextInput(attrs={"class": "myform", "size": 30}),
     )
 
 
+# This is the form for the Monte Carlo Simulator.
 class MonteCarloForm(forms.Form):
+    # Integer field for the number of simulations to run.
     sim_quantity = forms.IntegerField(
         label="Number of Simulations ",
+        # Validators to ensure the number of simulations is within an acceptable range.
         validators=[
             MinValueValidator(
                 1, message="The number of simulations must be at least 1."
@@ -48,10 +59,12 @@ class MonteCarloForm(forms.Form):
             ),
         ],
     )
+    # Floating point fields for the minimum and maximum values for the simulation.
     min_val = forms.FloatField(label="Minimum Value")
     max_val = forms.FloatField(label="Maximum Value")
+    # Floating point field for the target value in the simulation.
     target_val = forms.FloatField(label="Target Value")
-
+    # Optional integer field for the number of simulations for a second data range.
     second_sim_quantity = forms.IntegerField(
         required=False,
         label="Second Number of Simulations",
@@ -66,23 +79,33 @@ class MonteCarloForm(forms.Form):
             ),
         ],
     )
+    # Optional floating point fields for the minimum and maximum values for the second data range.
     second_min_val = forms.FloatField(label="Second Minimum Value", required=False)
     second_max_val = forms.FloatField(label="Second Maximum Value", required=False)
+    # Optional floating point field for the target value for the second data range.
     second_target_val = forms.FloatField(label="Second Target Value", required=False)
 
 
+# This is used to make sure the user enters a valid ZIP code.
 zdb = ZipCodeDatabase()
 
 
+# This is the form for the Weather Forecast app.
 class WeatherForm(forms.Form):
+    # Define a CharField for the ZIP code with a maximum length of 5 characters.
     zip_code = forms.CharField(label="ZIP Code:", max_length=5)
 
+    # Define a custom clean method for the zip_code field.
     def clean_zip_code(self):
+        # Retrieve the zip_code from the cleaned_data dictionary.
         zip_code = self.cleaned_data["zip_code"]
         try:
-            zdb[zip_code]  # this will raise an exception if the zip code is invalid
+            # Attempt to access the zip_code in a ZIP code database (zdb).
+            # This line will raise an exception if the zip code is not found in the database.
+            zdb[zip_code]
         except (KeyError, IndexError):
             raise ValidationError(
                 "You've made an invalid submission. Please enter a valid ZIP code."
             )
+        # If no exception is raised, return the validated zip_code.
         return zip_code
--- projects/views.py
+++ projects/views.py
@@ -1,40 +1,42 @@
-from django.shortcuts import render
-from .forms import QRForm
+from django.shortcuts import render, redirect
+from .forms import QRForm, MonteCarloForm, WeatherForm, TextForm
 import os
 import qrcode
 from django.http import HttpResponse
-from .forms import WeatherForm
 import requests
 import json
 import datetime
 from pyzipcode import ZipCodeDatabase
 import numpy as np
 import matplotlib.pyplot as plt
-from .forms import MonteCarloForm
-from django.shortcuts import redirect
 from django.conf import settings
-from .forms import TextForm
 import textstat
 
 
+# This is the code for the Grade Level Analyzer.
 def grade_level_analyzer(request):
+    # Check if the request method is POST.
     if request.method == "POST":
+        # Initialize the form with data from the request.
         form = TextForm(request.POST)
+        # Validate the form.
         if form.is_valid():
+            # Extract the input text from the form.
             input_text = form.cleaned_data["text"]
+            # Calculate readability scores using different indices.
             results = {
                 "flesch_kincaid_grade": textstat.flesch_kincaid_grade(input_text),
                 "gunning_fog": textstat.gunning_fog(input_text),
                 "coleman_liau_index": textstat.coleman_liau_index(input_text),
             }
-            # Calculate the weighted average of the scores
+            # Calculate the weighted average of the scores.
             average_score = round(
                 (0.5 * results["flesch_kincaid_grade"])
                 + (0.3 * results["gunning_fog"])
                 + (0.2 * results["coleman_liau_index"]),
                 1,
             )
-            # Calculate the uniform average of the scores
+            # Calculate the uniform average of the scores.
             uniform_average_score = round(
                 (
                     results["flesch_kincaid_grade"]
@@ -44,142 +46,147 @@ def grade_level_analyzer(request):
                 / 3,
                 1,
             )
-            # Add the average scores to the results dictionary
+            # Add the calculated scores to the results dictionary.
             results["average_score"] = average_score
             results["uniform_average_score"] = uniform_average_score
-            # Define the context with results and form
+            # Prepare the context with the form and results for rendering.
             context = {"form": form, "results": results}
+            # Render the results page with the context.
             return render(request, "projects/grade_level_results.html", context)
         else:
-            # Form is not valid, re-render the page with the form
+            # If the form is invalid, re-render the page with the form.
             return render(request, "projects/grade_level_analyzer.html", {"form": form})
     else:
-        # GET request, so create a new form and render the page
+        # If the request is not POST, create a new form and render the page.
         form = TextForm()
         return render(request, "projects/grade_level_analyzer.html", {"form": form})
 
 
+# This is the code for the view for my website's robots.txt file.
 def robots_txt(request):
-    # Construct the absolute path to the robots.txt file
+    # Construct the absolute path to the robots.txt file.
     robots_txt_path = os.path.join(settings.BASE_DIR, "robots.txt")
-    # Open and read the content of the robots.txt file
+    # Open and read the content of the robots.txt file.
     with open(robots_txt_path, "r") as f:
         robots_txt_content = f.read()
     # Return the content as HttpResponse with content type 'text/plain'
     return HttpResponse(robots_txt_content, content_type="text/plain")
 
 
+# This is the code for the view for the txt file containing my website's required Python libraries.
 def requirements_txt(request):
-    # Construct the absolute path to the requirements.txt file
+    # Construct the absolute path to the requirements.txt file.
     requirements_txt_path = os.path.join(settings.BASE_DIR, "requirements.txt")
-    # Open and read the content of the requirements.txt file
+    # Open and read the content of the requirements.txt file.
     with open(requirements_txt_path, "r") as f:
         requirements_txt_content = f.read()
     # Return the content as HttpResponse with content type 'text/plain'
     return HttpResponse(requirements_txt_content, content_type="text/plain")
 
 
+# This is the code for the viwe for the view for the txt file containing my website's runtime.
 def runtime_txt(request):
-    # Construct the absolute path to the runtime.txt file
+    # Construct the absolute path to the runtime.txt file.
     runtime_txt_path = os.path.join(settings.BASE_DIR, "runtime.txt")
-    # Open and read the content of the runtime.txt file
+    # Open and read the content of the runtime.txt file.
     with open(runtime_txt_path, "r") as f:
         runtime_txt_content = f.read()
-    # Return the content as HttpResponse with content type 'text/plain'
+    # Return the content as HttpResponse with content type 'text/plain'.
     return HttpResponse(runtime_txt_content, content_type="text/plain")
 
 
-"""
-def robots_txt(request):
-    # Open and read the content of your robots.txt file
-    with open("/robots.txt", "r") as f:
-        robots_txt_content = f.read()
-    # Return the content as HttpResponse with content type 'text/plain'
-    return HttpResponse(robots_txt_content, content_type="text/plain")
-"""
-
-
+# This is the code for my 404 catcher.  It returns the root, or homepage, of my website.
 def view_404(request, exception):
     return redirect("/")
 
 
-"""
-def home(request):
-    # Set the canonical URL
-    canonical_url = "https://www.bencritt.net/"
-
-    # Render the template
-    context = {}
-    rendered_template = render(request, "projects/home.html", context)
-
-    # Create the HTTP response
-    response = HttpResponse(rendered_template)
-
-    # Set the canonical URL in the HTTP header
-    response["Link"] = '<{}>; rel="canonical"'.format(canonical_url)
-
-    return response
-"""
-
-
+# This is the code for my homepage.  It's set in URL paths to the root of my website.
 def home(request):
     return render(request, "projects/home.html")
 
 
+# This is the code for the page holding links to my résumé.
 def resume(request):
     return render(request, "projects/resume.html")
 
 
+# This is the code for the QR Code Generator.
 def qr_code_generator(request):
+    # Check if the request method is POST.
     if request.method == "POST":
+        # Initialize the form with data from the request.
         form = QRForm(request.POST)
+        # Validate the form.
         if form.is_valid():
+            # Extract the data to be encoded in the QR code.
             data = form.cleaned_data["qr_text"]
+            # Initialize a QR Code generator.
             qr = qrcode.QRCode(version=1, box_size=10, border=5)
+            # Add the data to the QR Code.
             qr.add_data(data)
+            # Optimize the QR code layout.
             qr.make(fit=True)
+            # Create an image from the QR Code instance.
             img = qr.make_image(fill_color="black", back_color="white")
+            # Determine the directory to save the QR code image.
             home_dir = os.path.expanduser("~")
             save_dir = os.path.join(home_dir, "Downloads")
+            # Create the directory if it doesn't exist.
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
+            # Define the filename and full path for the QR code image.
             filename = "qrcode.png"
             full_path = os.path.join(save_dir, filename)
+            # Save the QR code image to the specified path.
             img.save(full_path)
 
+            # Serve the QR code image as a downloadable file in the response.
             with open(full_path, "rb") as f:
                 response = HttpResponse(f.read(), content_type="image/png")
                 response["Content-Disposition"] = 'attachment; filename="qrcode.png"'
                 return response
+    # Handle non-POST requests by initializing an empty form.
     else:
         form = QRForm
+    # Render the QR code generator page with the form.
     return render(request, "projects/qr_code_generator.html", context={"form": form})
 
 
+# This is the code for the page containing methods of contacting me.
 def contact(request):
     return render(request, "projects/contact.html")
 
 
+# This is the code for the Monte Carlo Simulator.
 def monte_carlo_simulator(request):
+    # Check if the request method is POST.
     if request.method == "POST":
+        # Initialize the form with data from the request.
         form = MonteCarloForm(request.POST)
+        # Validate the form
         if form.is_valid():
             # This determines where to save the PDF file that will eventually be created.
             home_dir = os.path.expanduser("~")
             save_dir = os.path.join(home_dir, "Downloads")
+            # Create the directory if it doesn't exist.
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
+            # Create a filename and define the full path of the directory on the user's computer.
             filename = "probability_graph.pdf"
             full_path = os.path.join(save_dir, filename)
 
-            # This pulls the data from the HTML to prepare for the graph generation.
+            # This pulls the data from the first HTML form to prepare for the graph generation.
             sim_quantity = form.cleaned_data["sim_quantity"]
             min_val = form.cleaned_data["min_val"]
             max_val = form.cleaned_data["max_val"]
             target_val = form.cleaned_data["target_val"]
 
+            # Generate random data for the first simulation.
             sim_result = np.random.uniform(min_val, max_val, sim_quantity)
+
+            # Check for a second simulation.
             second_sim_quantity = form.cleaned_data["second_sim_quantity"]
+
             # Begin second data range, if there is one.
             if form.cleaned_data["second_sim_quantity"] is not None:
                 second_min_val = form.cleaned_data["second_min_val"]
@@ -206,19 +213,21 @@ def monte_carlo_simulator(request):
                     )
                 plt.savefig(full_path, format="pdf")
 
+                # Generate a response with the generated PDF.
                 with open(full_path, "rb") as f:
                     response = HttpResponse(f.read(), content_type="pdf")
                     response["Content-Disposition"] = (
                         'attachment; filename="probability_graph.pdf"'
                     )
                     return response
-
+            # Handle the case where there is only one simulation.
             elif form.cleaned_data["second_sim_quantity"] is None:
                 plt.figure()
                 plt.hist(sim_result, density=True, edgecolor="white")
                 plt.axvline(target_val, color="r")
                 plt.savefig(full_path, format="pdf")
 
+                # Generate a response with the generated PDF.
                 with open(full_path, "rb") as f:
                     response = HttpResponse(f.read(), content_type="pdf")
                     response["Content-Disposition"] = (
@@ -226,53 +235,77 @@ def monte_carlo_simulator(request):
                     )
                     return response
 
+    # Create an empty form for GET request.
     else:
         form = MonteCarloForm()
 
+    # Render the Monte Carlo simulator page with the form.
     return render(
         request, "projects/monte_carlo_simulator.html", context={"form": form}
     )
 
 
+# This is the code for the Weather Forecast app.
 def get_coordinates(zip_code):
+    # API key for accessing the Google Geocoding API.
     API_KEY_LOCATION = "AIzaSyD0xBXRANSgMPe8HvaE2rSmm7u8E8QYAyM"
+    # Construct the API URL with the zip code and API key.
     API_URL = f"https://maps.googleapis.com/maps/api/geocode/json?address={zip_code}&key={API_KEY_LOCATION}"
+    # Send a GET request to the Google Geocoding API.
     response = requests.get(API_URL)
+    # Parse the JSON response content.
     data = json.loads(response.content)
+    # Check if the API call was successful and if results were found.
     if response.status_code == 200 and data["status"] == "OK":
+        # Extract the location data (latitude and longitude).
         location = data["results"][0]["geometry"]["location"]
+        # Return the latitude and longitude as a tuple.
         return location["lat"], location["lng"]
     else:
+        # Return None if the API call was unsuccessful or if no results were found.
         return None
 
 
+# This is the code for the Weather Forecast app.
 def get_city_and_state(zip_code):
+    # API key for accessing the Google Geocoding API.
     API_KEY_CITY = "AIzaSyD0xBXRANSgMPe8HvaE2rSmm7u8E8QYAyM"
+    # Construct the API URL with the zip code and API key.
     url = f"https://maps.googleapis.com/maps/api/geocode/json?address={zip_code}&key={API_KEY_CITY}"
+    # Send a GET request to the Google Geocoding API.
     response = requests.get(url)
+    # Parse the JSON response content.
     data = json.loads(response.content)
+    # Check if the API call was successful and if results were found.
     if response.status_code == 200 and data["status"] == "OK":
+        # Extract the city name from the response.
         city_name = data["results"][0]["address_components"][1]["long_name"]
+        # Extract the state name from the response.
         state_name = data["results"][0]["address_components"][3]["long_name"]
+        # Return the city and state names as a tuple.
         return city_name, state_name
     else:
+        # Return None if the API call was unsuccessful or if no results were found.
         return None
 
 
 zdb = ZipCodeDatabase()
 
 
+# This is the code for the Weather Forecast app.
 def weather(request):
-    # Create a new form instance.
+    # Initialize the weather form, allowing for POST or None (for GET requests).
     form = WeatherForm(request.POST or None)
 
-    # Get the coordinates, even if the form is not valid.
+    # Retrieve the zip code from the POST request, if present.
     if request.method == "POST":
         zip_code = request.POST["zip_code"]
 
     # If the form is valid, process the form data and render the weather forecast.
     if form.is_valid():
+        # Obtain the coordinates for the given zip code.
         coordinates = get_coordinates(zip_code)
+        # Handle cases where coordinates cannot be found.
         if coordinates == None:
             context = {
                 "form": form,
@@ -280,23 +313,19 @@ def weather(request):
             }
             return render(request, "projects/weather.html", context)
         else:
+            # Retrieve city and state names based on the zip code.
             city_name, state_name = get_city_and_state(zip_code)
             latitude, longitude = coordinates
+        # API key for accessing the weather information.
         API_KEY_WEATHER = "7e805bf42d5f1713e20456904be7155c"
-        '''"49372bd312fbced940386aa2826ada9d"'''
+        # Construct the API URL with coordinates and API key.
         API_URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={API_KEY_WEATHER}&units=imperial"
+        # Send a GET request to the weather API.
         response = requests.get(API_URL)
         data = json.loads(response.content)
+        # Parse and extract current weather information.
         icon_code_current = data["current"]["weather"][0]["icon"]
         icon_url_current = f"https://openweathermap.org/img/wn/{icon_code_current}.png"
-        """
-        # I can currently only get icons working with current weather.
-        # This fix will be on my to-do list.
-        icon_codes_daily = []
-        for day in data["daily"]:
-            icon_codes_daily.append(day["weather"][0]["icon"])
-        icon_url_daily = f"https://openweathermap.org/img/wn/{icon_codes_daily}.png"
-        """
         current_weather = data["current"]
         day = data["daily"]
         current_weather_report = []
@@ -323,31 +352,7 @@ def weather(request):
                 ),
             }
         )
-        """
-        # I want to eventually add a Moon phase feature.
-        # I can do this by reporting phase numbers.
-        # I want to report descriptive strings instead of numbers.
-        # This will take some time.
-        # I will likely devote this feature to it's own app, since it's outside the scope of "weather."
-        moon_phase = data["daily"][0]["moon_phase"]
-
-        if moon_phase == 1 or moon_phase == 0:
-            moon_phase = "New Moon"
-        elif moon_phase == 0.25:
-            moon_phase = "First Quarter Moon"
-        elif moon_phase == 0.50:
-            moon_phase = "Full Moon"
-        elif moon_phase == 0.75:
-            moon_phase = "Last Quarter Moon"
-        elif moon_phase < 0.25:
-            moon_phase = "Waxing Crescent Moon"
-        elif 0.25 < moon_phase < 0.5:
-            moon_phase = "Waxing Gibbous Moon"
-        elif 0.5 < moon_phase < 0.75:
-            moon_phase = "Waning Gibbous Moon"
-        elif 0.75 < moon_phase < 1:
-            moon_phase = "Waning Crescent Moon"
-        """
+        # Parse and extract daily weather forecast information.
         daily_forecast = []
         for day in data["daily"]:
             daily_forecast.append(
@@ -369,24 +374,20 @@ def weather(request):
                     "dew_point": day["dew_point"],
                     "humidity": day["humidity"],
                     "precipitation_chance": day["pop"],
-                    # "icon_url_daily": icon_url_daily,
-                    # "moon:phase": moon_phase,
-                    # "moon_phase": day["moon_phase"],
-                    # "moonrise": datetime.datetime.fromtimestamp(day["moonrise"]),
-                    # "moonset": datetime.datetime.fromtimestamp(day["moonset"]),
                 }
             )
 
+        # Prepare context with weather and location data for rendering.
         context = {
             "daily_forecast": daily_forecast,
             "city_name": city_name,
             "state_name": state_name,
             "current_weather_report": current_weather_report,
         }
-
+        # Render the page with weather results.
         return render(request, "projects/weather_results.html", context)
 
-    # If the form is not valid, render the form again.
+    # If the form is not valid or it's a GET request, render the form again.
     else:
         context = {
             "form": form,
@@ -394,5 +395,6 @@ def weather(request):
         return render(request, "projects/weather.html", context)
 
 
+# This is the code for the page containing information on all of my projects.
 def all_projects(request):
     return render(request, "projects/all_projects.html")
