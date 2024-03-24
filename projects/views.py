963ecabaad592689810f91288929d65ebb764b9c
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-03-24 14:10:25 -0500

    Improving PWA load screen.

--- projects/views.py
+++ projects/views.py
@@ -112,7 +112,7 @@ def manifest(request):
         "start_url": "/",
         "display": "standalone",
         "theme_color": "#000000",
-        "background_color": "#ffffff",
+        "background_color": "#000000",
     }
     return JsonResponse(manifest_json)
 
