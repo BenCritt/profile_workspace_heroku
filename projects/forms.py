bf3c9be398b0723727554570555a8af1d1901985
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-08-25 16:56:13 -0500

    SEO Improvements

--- projects/forms.py
+++ projects/forms.py
@@ -37,7 +37,7 @@ class TextForm(forms.Form):
 class QRForm(forms.Form):
     # Create a CharField to hold the text input for the QR code.
     qr_text = forms.CharField(
-        label="",  # The label is set to an empty string for a cleaner UI without a field label.
+        label="",
         max_length=8000,  # Set the maximum length of the text input to 8000 characters.
         # Configure the TextInput widget with a custom class for styling and set the size attribute.
         widget=forms.TextInput(attrs={"class": "myform", "size": 30}),
