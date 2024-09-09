09826abd2518cc99a8cea191546273458fc1a156
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-09-09 05:15:17 -0500

    Turning DEBUG to False Removing commented code that I moved from views.py to utils.py

--- projects/views.py
+++ projects/views.py
@@ -288,52 +288,6 @@ def monte_carlo_simulator(request):
     )
 
 
-"""
-# This is the code for the Weather Forecast app.
-def get_coordinates(zip_code):
-    # API key for accessing the Google Geocoding API.
-    API_KEY_LOCATION = "AIzaSyD0xBXRANSgMPe8HvaE2rSmm7u8E8QYAyM"
-    # Construct the API URL with the zip code and API key.
-    API_URL = f"https://maps.googleapis.com/maps/api/geocode/json?address={zip_code}&key={API_KEY_LOCATION}"
-    # Send a GET request to the Google Geocoding API.
-    response = requests.get(API_URL)
-    # Parse the JSON response content.
-    data = json.loads(response.content)
-    # Check if the API call was successful and if results were found.
-    if response.status_code == 200 and data["status"] == "OK":
-        # Extract the location data (latitude and longitude).
-        location = data["results"][0]["geometry"]["location"]
-        # Return the latitude and longitude as a tuple.
-        return location["lat"], location["lng"]
-    else:
-        # Return None if the API call was unsuccessful or if no results were found.
-        return None
-
-
-# This is the code for the Weather Forecast app.
-def get_city_and_state(zip_code):
-    # API key for accessing the Google Geocoding API.
-    API_KEY_CITY = "AIzaSyD0xBXRANSgMPe8HvaE2rSmm7u8E8QYAyM"
-    # Construct the API URL with the zip code and API key.
-    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={zip_code}&key={API_KEY_CITY}"
-    # Send a GET request to the Google Geocoding API.
-    response = requests.get(url)
-    # Parse the JSON response content.
-    data = json.loads(response.content)
-    # Check if the API call was successful and if results were found.
-    if response.status_code == 200 and data["status"] == "OK":
-        # Extract the city name from the response.
-        city_name = data["results"][0]["address_components"][1]["long_name"]
-        # Extract the state name from the response.
-        state_name = data["results"][0]["address_components"][3]["long_name"]
-        # Return the city and state names as a tuple.
-        return city_name, state_name
-    else:
-        # Return None if the API call was unsuccessful or if no results were found.
-        return None
-"""
-
-
 # This is the code for the Weather Forecast app.
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def weather(request):
