0ecfde0adf96d2363148c7156e7c41899b1cbd16
BenCritt <130415312+BenCritt@users.noreply.github.com>
2024-10-13 18:06:29 -0500

    Adding Form logic for invalid submissions.

--- projects/forms.py
+++ projects/forms.py
@@ -15,6 +15,16 @@ from urllib.parse import urlparse
 class CarrierSearchForm(forms.Form):
     search_value = forms.CharField(label="Enter USDOT Number", max_length=50)
 
+    # Handle an invalid submission.
+    def clean_search_value(self):
+        data = self.cleaned_data["search_value"]
+
+        # Ensure the USDOT number is numeric
+        if not data.isdigit():
+            raise forms.ValidationError("Please enter a valid numeric USDOT number.")
+
+        return data
+
 
 # This is the form for the Grade Level Text Analyzer.
 class TextForm(forms.Form):
