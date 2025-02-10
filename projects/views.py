ec91deff7fb5f7d628807ae32f0ba6bc9146635b
BenCritt <130415312+BenCritt@users.noreply.github.com>
2025-02-09 18:51:19 -0600

    Reversing 404 improvements.

--- projects/views.py
+++ projects/views.py
@@ -699,7 +699,16 @@ def runtime_txt(request):
 
 # This is the code for my 404 catcher.  It returns the root, or homepage, of my website.
 def view_404(request, exception):
-    return render(request, "404.html", status=404)
+    # If the request is for a static file (including the service worker)
+    if (
+        request.path.startswith(settings.STATIC_URL)
+        or request.path == "/service-worker.js"
+    ):
+        # Return the default 404 response for static files
+        return HttpResponseNotFound("File not found")
+
+    # Otherwise, redirect to the homepage
+    return redirect("/")
 
 
 # This is the code for my homepage.  It's set in URL paths to the root of my website.
