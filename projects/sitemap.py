31cb1b43b65a25448bda6d20c2ce2d7b6791420f
Ben Crittenden <bencritt89@gmail.com>
2025-05-31 23:12:21 -0500

    Debugging a server error.

--- projects/sitemap.py
+++ projects/sitemap.py
@@ -5,7 +5,10 @@ from django.contrib.sitemaps import Sitemap
 class RootSitemap(Sitemap):
     priority = 1.0
     changefreq = "daily"
-    items = ("projects:home",)
+
+    # MUST be a method
+    def items(self):
+        return ["projects:home"]
 
     def location(self, item):
         return reverse(item)
@@ -14,21 +17,23 @@ class RootSitemap(Sitemap):
 class StaticViewSitemap(Sitemap):
     priority = 0.5
     changefreq = "weekly"
-    items = (
-        "projects:all_projects",
-        "projects:qr_code_generator",
-        "projects:monte_carlo_simulator",
-        "projects:weather",
-        "projects:grade_level_analyzer",
-        "projects:dns_tool",
-        "projects:ip_tool",
-        "projects:ssl_check",
-        "projects:it_tools",
-        "projects:freight_safety",
-        "projects:seo_head_checker",
-        "projects:iss_tracker",
-        "projects:xml_splitter",
-    )
+
+    def items(self):
+        return [
+            "projects:all_projects",
+            "projects:qr_code_generator",
+            "projects:monte_carlo_simulator",
+            "projects:weather",
+            "projects:grade_level_analyzer",
+            "projects:dns_tool",
+            "projects:ip_tool",
+            "projects:ssl_check",
+            "projects:it_tools",
+            "projects:freight_safety",
+            "projects:seo_head_checker",
+            "projects:iss_tracker",
+            "projects:xml_splitter",      # underscore, not hyphen
+        ]
 
     def location(self, item):
         return reverse(item)
