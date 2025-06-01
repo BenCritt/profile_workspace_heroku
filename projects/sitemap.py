2701b2e41c3e439cf1127914daec127748c13586
Ben Crittenden <bencritt89@gmail.com>
2025-05-31 23:06:04 -0500

    Fixing sitemap

--- projects/sitemap.py
+++ projects/sitemap.py
@@ -1,51 +1,34 @@
-# Import necessary Django modules and classes for sitemaps.
-from django.contrib import sitemaps
 from django.urls import reverse
 from django.contrib.sitemaps import Sitemap
 
 
-# Define a sitemap for the root URL of the website.
 class RootSitemap(Sitemap):
-    # Set the priority to the maximum (1.0) as this is the homepage.
     priority = 1.0
-    # Indicate that the homepage changes daily.
     changefreq = "daily"
+    items = ("projects:home",)
 
-    # Define the items that will be included in this sitemap - only the homepage in this case.
-    def items(self):
-        return ["home"]  # This is the homepage.
-
-    # Define how to determine the location (URL) for each item in the sitemap.
     def location(self, item):
-        return "/"  # This is the root URL
+        return reverse(item)
 
 
-# Define a sitemap for static views within the site.
-class StaticViewSitemap(sitemaps.Sitemap):
-    # Set a default priority for these pages, lower than the homepage.
+class StaticViewSitemap(Sitemap):
     priority = 0.5
-    # These pages are expected to change weekly.
     changefreq = "weekly"
+    items = (
+        "projects:all_projects",
+        "projects:qr_code_generator",
+        "projects:monte_carlo_simulator",
+        "projects:weather",
+        "projects:grade_level_analyzer",
+        "projects:dns_tool",
+        "projects:ip_tool",
+        "projects:ssl_check",
+        "projects:it_tools",
+        "projects:freight_safety",
+        "projects:seo_head_checker",
+        "projects:iss_tracker",
+        "projects:xml_splitter",
+    )
 
-    # List the named URL patterns for static pages to be included in the sitemap.
-    def items(self):
-        return [
-            "all_projects",
-            "qr_code_generator",
-            "monte_carlo_simulator",
-            "weather",
-            "grade_level_analyzer",
-            "dns_tool",
-            "ip_tool",
-            "ssl_check",
-            "it_tools",
-            "freight_safety",
-            "seo_head_checker",
-            "iss_tracker",
-            "xml-splitter",
-        ]
-
-    # Define how to determine the location (URL) for each item, using the 'reverse' function to find URLs by their name.
     def location(self, item):
-        # Dynamically create URLs for each item.
-        return reverse("projects:" + item)
+        return reverse(item)
