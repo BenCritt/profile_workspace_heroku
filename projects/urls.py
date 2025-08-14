ef7bdff6d2a7f81da2921acbf7ba0b86081b815f
Ben Crittenden <bencritt89@gmail.com>
2025-08-13 19:53:08 -0500

    Removing defunct runtime URL pattern.

--- projects/urls.py
+++ projects/urls.py
@@ -38,7 +38,6 @@ urlpatterns = [
     path("projects/it-tools/", views.it_tools, name="it_tools"),
     path("robots.txt", robots_txt, name="robots_txt"),
     path("requirements.txt", requirements_txt, name="requirements_txt"),
-    path("runtime.txt", runtime_txt, name="runtime_txt"),
     path("", views.home, name="home"),
     path("projects/weather/", views.weather, name="weather"),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
