d573d792435d973f07dfdd1dcfab0795da6d0a04
Ben Crittenden <bencritt89@gmail.com>
2025-08-16 13:01:54 -0500

    Troubleshooting a server error.

--- projects/views.py
+++ projects/views.py
@@ -420,6 +420,7 @@ def manifest(request):
 
 
 # This is the code for the Freight Carrier Safety Reporter
+# Disallow caching to prevent CSRF token errors.
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def freight_safety(request):
     from .freight_utils import get_fmcsa_carrier_data_by_usdot
@@ -468,6 +469,7 @@ def freight_safety(request):
 
 
 # This is the code for the Grade Level Analyzer.
+# Disallow caching to prevent CSRF token errors.
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def grade_level_analyzer(request):
     # Check if the request method is POST.
@@ -601,58 +603,6 @@ def grade_level_analyzer(request):
         form = TextForm()
         return render(request, "projects/grade_level_analyzer.html", {"form": form})
 
-
-'''
-# This is the code for the Grade Level Analyzer.
-@cache_control(no_cache=True, must_revalidate=True, no_store=True)
-def grade_level_analyzer(request):
-    # Check if the request method is POST.
-    if request.method == "POST":
-        # Initialize the form with data from the request.
-        form = TextForm(request.POST)
-        # Validate the form.
-        if form.is_valid():
-            # Extract the input text from the form.
-            input_text = form.cleaned_data["text"]
-            # Calculate readability scores using different indices.
-            results = {
-                "flesch_kincaid_grade": textstat.flesch_kincaid_grade(input_text),
-                "gunning_fog": textstat.gunning_fog(input_text),
-                "coleman_liau_index": textstat.coleman_liau_index(input_text),
-            }
-            # Calculate the weighted average of the scores.
-            average_score = round(
-                (0.5 * results["flesch_kincaid_grade"])
-                + (0.3 * results["gunning_fog"])
-                + (0.2 * results["coleman_liau_index"]),
-                1,
-            )
-            # Calculate the uniform average of the scores.
-            uniform_average_score = round(
-                (
-                    results["flesch_kincaid_grade"]
-                    + results["gunning_fog"]
-                    + results["coleman_liau_index"]
-                )
-                / 3,
-                1,
-            )
-            # Add the calculated scores to the results dictionary.
-            results["average_score"] = average_score
-            results["uniform_average_score"] = uniform_average_score
-            # Prepare the context with the form and results for rendering.
-            context = {"form": form, "results": results}
-            # Render the results page with the context.
-            return render(request, "projects/grade_level_results.html", context)
-        else:
-            # If the form is invalid, re-render the page with the form.
-            return render(request, "projects/grade_level_analyzer.html", {"form": form})
-    else:
-        # If the request is not POST, create a new form and render the page.
-        form = TextForm()
-        return render(request, "projects/grade_level_analyzer.html", {"form": form})
-'''
-    
 
 # This is the code for the view for my website's llms.txt file.
 def llms_txt(request):
